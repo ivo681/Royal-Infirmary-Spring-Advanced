37a7c5eae1b8ac3899926fe0b595bfc13d6841d2
ivo681 <ivo.marinov.nikolov@outlook.com>
2022-07-16 19:22:07 +0300

    Revert "connection test"
    This reverts commit e1222c292d3fac56bf4fe215644b16dbd2883fac.

--- src/main/java/com/example/webmoduleproject/WebModuleProjectApplication.java
+++ src/main/java/com/example/webmoduleproject/WebModuleProjectApplication.java
@@ -9,7 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 @SpringBootApplication
 public class WebModuleProjectApplication {
-    //v16/07/2022
 
     public static void main(String[] args) {
         SpringApplication.run(WebModuleProjectApplication.class, args);
